78881ba9170f8caeaf7a590956d33e8ed938937d
cli/asgard <lumos.leee@gmail.com>
2020-10-24 08:50:19 -0700

    add eigen chemistry solver and jupiter pgen

--- src/chemistry/chemistry_solver.hpp
+++ src/chemistry/chemistry_solver.hpp
@@ -1,10 +1,20 @@
 #ifndef CHEMISTRY_SOLVER_HPP
 #define CHEMISTRY_SOLVER_HPP
 
-#include "../math/linalg.h"
+//#include "../math/linalg.h"
+// Athena++ headers
+#include "../athena.hpp"
+
+// Eigen header files
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/LU>
 
 class ChemistrySolver {
 public:
+  // needed for Eigen small matrix
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
   ChemistrySolver() {}
 
   // solve independent chemistry: A*dq = b
@@ -15,24 +25,30 @@ public:
       #pragma ivdep
       for (int j = 0; j < NPHASE; ++j) {
         for (int k = 0; k < NPHASE; ++k)
-          a_[j][k] = A[1+j*NVAPOR+i][1+k*NVAPOR+i];
-        sol_[j] = dq[1+j*NVAPOR+i];
+          a_(j,k) = A[1+j*NVAPOR+i][1+k*NVAPOR+i];
+        sol_(j) = dq[1+j*NVAPOR+i];
       }
 
-      ludcmp<NPHASE>(a_, indx_, vv_);
-      lubksb<NPHASE>(a_, indx_, sol_);
+      //ludcmp<NPHASE>(a_, indx_, vv_);
+      //lubksb<NPHASE>(a_, indx_, sol_);
+
+      if (NPHASE <= 4) sol_ = a_.inverse()*sol_;
+      else sol_ = a_.partialPivLu().solve(sol_);
 
       for (int j = 0; j < NPHASE; ++j)
-        dq[1+j*NVAPOR+i] = sol_[j];
+        dq[1+j*NVAPOR+i] = sol_(j);
     }
     dq[0] = 0.;
   }
 
 private:
   // scratch arrays
-  Real a_[NPHASE][NPHASE];
-  Real sol_[NPHASE], vv_[NPHASE];
-  int indx_[NPHASE];
+  Eigen::Matrix<Real, NPHASE, NPHASE> a_;
+  Eigen::Matrix<Real, NPHASE, 1> sol_;
+
+  //Real a_[NPHASE][NPHASE];
+  //Real sol_[NPHASE], vv_[NPHASE];
+  //int indx_[NPHASE];
 };
 
 #endif
